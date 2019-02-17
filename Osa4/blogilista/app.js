e02ac85a698ed6b9e9a9620c665b52d272d1e831
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2019-02-17 21:20:28 +0200

    blogs.js

--- Osa4/blogilista/app.js
+++ Osa4/blogilista/app.js
@@ -1,5 +1,5 @@
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -12,22 +12,6 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true })
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
