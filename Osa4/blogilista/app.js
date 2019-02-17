98848e5cac1b69120c60dcacee418deb5ca23395
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2019-02-17 21:15:23 +0200

    blog.js

--- Osa4/blogilista/app.js
+++ Osa4/blogilista/app.js
@@ -1,18 +1,10 @@
 const config = require('./utils/config')
+const Blog = require('./models/blog')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
