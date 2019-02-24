4b9e9b7a2fe730856f96de04d8ddd1adc9e2c9cf
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2019-02-24 05:17:58 +0200

    Blog.js päivitetty

--- Osa5/bloglist-frontend/src/components/Blog.js
+++ Osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  const toggleVisible = () => {
+    setVisible(!visible)
+  }
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => toggleVisible()}>
+        {blog.title}, by {blog.author}
+      </div>
+      <div style={showWhenVisible}>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          {blog.likes}
+          <button>like</button>
+        </div>
+        <div>
+          added by {blog.user.name}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
